a928826ff5c1b2877a95a91c8fdf6e2a4a2e1304
Steven Silvester <steven.silvester@ieee.org>
2016-12-21 15:39:38 -0600

    Switch to setup.py and Makefile so we can include m-files

--- Makefile
+++ Makefile
@@ -1,24 +1,29 @@
-# Note: This is meant for developer use only
-.PHONY: all clean test release 
+# Note: This is meant for octave_kernel developer use only
+.PHONY: all clean test release
 
-export TEST_ARGS=--exe -v --with-doctest
 export NAME=octave_kernel
 export VERSION=`python -c "import $(NAME); print($(NAME).__version__)"`
 
 all: clean
-	flit install --symlink
+	python setup.py install
 
 clean:
 	rm -rf build
 	rm -rf dist
-	find . -name "*.pyc" -o -name "*.py,cover"| xargs rm -f
 
 test: clean
+	pip install jupyter_kernel_test
+	python setup.py build
 	python test_octave_kernel.py
+	make clean
 
 release: clean
 	pip install wheel
-	flit wheel --upload
+	python setup.py register
+	rm -rf dist
+	python setup.py bdist_wheel --universal
+	python setup.py sdist
 	git tag v$(VERSION)
 	git push origin --all
 	git push origin --tags
+	twine upload dist/*
---
+++ setup.py
@@ -0,0 +1,54 @@
+"""Setup script for octave_kernel package.
+"""
+DISTNAME = 'octave_kernel'
+DESCRIPTION = 'A Jupyter kernel for Octave.'
+LONG_DESCRIPTION = open('README.rst', 'rb').read().decode('utf-8')
+MAINTAINER = 'Steven Silvester'
+MAINTAINER_EMAIL = 'steven.silvester@ieee.org'
+URL = 'http://github.com/calsto/octave_kernel'
+LICENSE = 'MIT'
+REQUIRES = ["metakernel (>0.16.0)"]
+PACKAGES = []
+PACKAGE_DATA = {DISTNAME: ['*.m']}
+CLASSIFIERS = """\
+Intended Audience :: Science/Research
+License :: OSI Approved :: BSD License
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
+Topic :: Scientific/Engineering
+Topic :: Software Development
+Topic :: System :: Shells
+"""
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('octave_kernel/__init__.py', 'rb') as fid:
+    for line in fid:
+        line = line.decode('utf-8')
+        if line.startswith('__version__'):
+            version = line.strip().split()[-1][1:-1]
+            break
+
+
+setup(
+    name=DISTNAME,
+    version=version,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    packages=PACKAGES,
+    package_data=PACKAGE_DATA,
+    url=URL,
+    download_url=URL,
+    license=LICENSE,
+    platforms=["Any"],
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=list(filter(None, CLASSIFIERS.split('\n'))),
+    requires=REQUIRES
+ )
