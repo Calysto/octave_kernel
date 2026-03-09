8568f75916cc42a631dea2ec67fe7fc27af914db
Steven Silvester <steven.silvester@gmail.com>
2026-03-08 20:23:24 -0500

    clean up startup

--- .binder/jupyterhub_config.py
+++ .binder/jupyterhub_config.py
@@ -1,15 +1,6 @@
 # ruff: noqa: F821
-c.Spawner.environment = {"OCTAVE_EXECUTABLE": "octave", "DISPLAY": ":99"}
+c.Spawner.environment = {"DISPLAY": ":99"}
 
-
-async def pre_spawn_hook(spawner):
-    import subprocess
-
-    subprocess.Popen(
-        ["Xvfb", ":99", "-screen", "0", "1024x768x24"],
-        stdout=open("/dev/null", "w"),
-        stderr=open("/dev/null", "w"),
-    )
-
-
-c.Spawner.pre_spawn_hook = pre_spawn_hook
+c.KubeSpawner.lifecycle_hooks = {
+    "postStart": {"exec": {"command": ["Xvfb", ":99", "-screen", "0", "1024x768x24"]}}
+}
