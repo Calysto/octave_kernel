9cb778fbf7b529c29dc1ee97efdf03f0bcd43e6f
Steven Silvester <steven.silvester@gmail.com>
2026-03-08 20:39:05 -0500

    fix start handling

--- .binder/jupyterhub_config.py
+++
@@ -1,6 +0,0 @@
-# ruff: noqa: F821
-c.Spawner.environment = {"DISPLAY": ":99"}
-
-c.KubeSpawner.lifecycle_hooks = {
-    "postStart": {"exec": {"command": ["Xvfb", ":99", "-screen", "0", "1024x768x24"]}}
-}
